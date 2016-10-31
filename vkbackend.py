d3760a102fa61b31c73673d6992453c1bc54dd00
Ax3 <axeeffect1234@gmail.com>
2016-10-31 16:59:43 +0000

    Fix crashing when no response given

--- vkbackend.py
+++ vkbackend.py
@@ -305,7 +305,10 @@ class VKBackend(ErrBot):
                 except ValueError:
                     response = None
                     pass
-                    
+
+                if response.get("ts", None):
+                    response = None    
+
                 if response:
 
                     self.pollServer = "http://{server}?act={act}&key={key}&ts={ts}&wait={wait}&mode={mode}".format(server=self.initresponse["server"], 
